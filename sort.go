a5db35d5f432f7f0bf5fb54cffe064abd96910c9
wangxinguo <wangxinguo@didichuxing.com>
2022-07-12 10:01:11 +0800

    修改

--- sort.go
+++ sort.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main()  {
-	nums := &[]int{5,3,9,6}
-	quickSort(nums, 0, 3)
-	fmt.Println(nums)
+	//nums := &[]int{5,3,9,6}
+	//quickSort(nums, 0, 3)
+
+	nums := []int64{8,4,5,7,1,3,6,2}
+	temp := make([]int64, len(nums))
+	mergeSort(nums, temp, 0, 7)
+	fmt.Println(temp)
 }
 
 func quickSort(nums *[]int, l, r int)  {
@@ -28,3 +32,42 @@ func quickSort(nums *[]int, l, r int)  {
 	quickSort(nums, start+1, r)
 }
 
+func mergeSort(nums, temp []int64, l, r int64)  {
+	if l < r {
+		mid := l + (r-l)/2
+		mergeSort(nums, temp, l, mid)
+		mergeSort(nums, temp, mid+1, r)
+		mergeList(&nums, &temp, l, mid, r)
+	}
+}
+
+func mergeList(nums, temp *[]int64, l, mid, r int64)  {
+	i, j, t := l, mid+1, 0
+	for i <= mid && j <= r {
+		if (*nums)[i] <= (*nums)[j] {
+			(*temp)[t] = (*nums)[i]
+			i ++
+			t ++
+		} else {
+			(*temp)[t] = (*nums)[j]
+			j ++
+			t ++
+		}
+	}
+
+	for i <= mid {
+		(*temp)[t] = (*nums)[i]
+		i ++
+		t ++
+	}
+
+	for j <= r {
+		(*temp)[t] = (*nums)[j]
+		j ++
+		t ++
+	}
+
+	for i:=int64(0); i<r; i++ {
+		(*nums)[i] = (*temp)[i]
+	}
+}
