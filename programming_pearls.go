05e62332c6bf80fda9eae8ee8bc2dd6c7eed9777
wangxinguo <wangxinguo@didichuxing.com>
2022-08-19 09:43:08 +0800

    修改

--- programming_pearls.go
+++ programming_pearls.go
@@ -3,13 +3,90 @@ package main
 import "fmt"
 
 func main()  {
-	s := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
-	reverseStr1(s, 3)
-	fmt.Println(s)
+	//s := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	//reverseStr1(s, 3)
+	//fmt.Println(s)
+	//return
+
+	r := GenCalendar(2022, 8)
+	fmt.Println(r)
 	return
 }
 
+type Date struct {
+	Year int
+	Month int
+	Day int
+}
 
+var monthDay = []int{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+
+func IsRunYear(year int) bool {
+	return (year % 4 ==0 && year % 100 != 0) || year % 400 == 0
+}
+
+func YearDay(date *Date) int {
+	totalDay := 0
+	for i:=1; i<date.Month; i++ {
+		totalDay += monthDay[i]
+	}
+	if date.Month > 2 && IsRunYear(date.Year) {
+		totalDay += 1
+	}
+	return totalDay
+}
+
+func BetweenDay(date1, date2 *Date) int {
+	if date1.Year == date2.Year {
+		return YearDay(date2) - YearDay(date1)
+	}
+	cnt := 0
+	for i:=date1.Year; i<date2.Year; i++ {
+		cnt += 365
+		if IsRunYear(i) {
+			cnt += 1
+		}
+	}
+	return cnt + YearDay(date2) - YearDay(date1)
+}
+
+func WeekDay(date *Date) int {
+	startDate := &Date{
+		Year:  1900,
+		Month: 1,
+		Day:   1,
+	}
+	return BetweenDay(startDate, date) % 7 + 1
+}
+
+func GenCalendar(year, month int) [][]int {
+	var r [][]int
+	startDate := &Date{
+		Year:  year,
+		Month: month,
+		Day:   1,
+	}
+	startDayWeekend := WeekDay(startDate)
+	lastDay := monthDay[month]
+	if IsRunYear(year) && month == 2 {
+		lastDay = 29
+	}
+	for i:=startDate.Day; i<=lastDay; i ++ {
+		tmpWeekDay := startDayWeekend
+		tmpArr := []int{}
+		for j:=1; j<=7; j++ {
+			if j < tmpWeekDay {
+				tmpArr = append(tmpArr, 0)
+				continue
+			}
+			tmpArr = append(tmpArr, tmpWeekDay)
+			tmpWeekDay ++
+		}
+		tmpWeekDay = 1
+		r = append(r, tmpArr)
+	}
+	return r
+}
 
 /**
 tmp = s[0]
