9df7fa948ef92cd35ec2d77057ae89e4cea3fafe
wangxinguo <wangxinguo@didichuxing.com>
2023-07-26 23:41:51 +0800

    添加代码

--- sort.go
+++ sort.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"sort"
 )
@@ -249,6 +250,61 @@ func max1(i, j int) int {
 		return i
 	}
 	return j
+}
+
+/**
+https://leetcode.cn/problems/top-k-frequent-words/description/
+ */
+
+type pair struct {
+	w string
+	c int
+}
+type hp []pair
+func (h hp) Len() int            { return len(h) }
+func (h hp) Less(i, j int) bool  { a, b := h[i], h[j]; return a.c < b.c || a.c == b.c && a.w > b.w }
+func (h hp) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *hp) Push(v interface{}) { *h = append(*h, v.(pair)) }
+func (h *hp) Pop() interface{}   { a := *h; v := a[len(a)-1]; *h = a[:len(a)-1]; return v }
+
+func topKFrequent1(words []string, k int) []string {
+	cnt := map[string]int{}
+	for _, w := range words {
+		cnt[w]++
+	}
+	h := &hp{}
+	for w, c := range cnt {
+		heap.Push(h, pair{w, c})
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	ans := make([]string, k)
+	for i := k - 1; i >= 0; i-- {
+		ans[i] = heap.Pop(h).(pair).w
+	}
+	return ans
+}
+
+func topKFrequent2(words []string, k int) []string {
+	mp := make(map[string]int)
+	for _, word := range words {
+		mp[word] ++
+	}
+	uniqueWords := make([]string, 0)
+	for v := range mp {
+		uniqueWords = append(uniqueWords, v)
+	}
+	sort.Slice(uniqueWords, func(i, j int) bool {
+		if mp[uniqueWords[i]] > mp[uniqueWords[j]] {
+			return true
+		}
+		if mp[uniqueWords[i]] == mp[uniqueWords[j]] && uniqueWords[i] < uniqueWords[j] {
+			return true
+		}
+		return false
+	})
+	return uniqueWords[:k]
 }
 
 /**
