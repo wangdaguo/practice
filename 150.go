2a689bae66023fc7573748be15158dedce25726a
wangxinguo <wangxinguo@didiglobal.com>
2024-05-10 10:06:59 +0800

    添加

--- 150.go
+++ 150.go
@@ -3123,5 +3123,15 @@ func singleNumber(nums []int) int {
 https://leetcode.cn/problems/single-number-ii/?envType=study-plan-v2&envId=top-interview-150
 */
 func singleNumber1(nums []int) int {
-
+	ans := int32(0)
+	for i := 0; i < 32; i++ {
+		total := int32(0)
+		for _, num := range nums {
+			total += int32(num) >> i & 1
+		}
+		if total%3 != 0 {
+			ans |= 1 << i
+		}
+	}
+	return int(ans)
 }
