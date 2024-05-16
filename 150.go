d1d7cb9a746ed8c39c0b786adecaad9f1f5d4451
wangxinguo <wangxinguo@didiglobal.com>
2024-05-16 10:12:22 +0800

    添加

--- 150.go
+++ 150.go
@@ -3263,3 +3263,21 @@ func myPow(x float64, n int) float64 {
 	res := myPow(x, n%2)
 	return half * half * res
 }
+
+/*
+*
+70. 爬楼梯
+https://leetcode.cn/problems/climbing-stairs/?envType=study-plan-v2&envId=top-interview-150
+*/
+func climbStairs(n int) int {
+	if n <= 2 {
+		return n
+	}
+	dp := make(map[int]int)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
