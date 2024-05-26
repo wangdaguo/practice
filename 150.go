d32402a663268d1ab6b183bc7c9b984d15ad62b5
wangxinguo <wangxinguo@didiglobal.com>
2024-05-26 11:14:44 +0800

    添加

--- 150.go
+++ 150.go
@@ -128,7 +128,8 @@ func main() {
 	//r := addBinary("11", "1")
 	//r := mySqrt(1)
 	//r := rob([]int{1, 2, 3, 1})
-	r := coinChange([]int{2}, 3)
+	//r := coinChange([]int{2}, 3)
+	r := lengthOfLIS([]int{1, 3, 6, 7, 9, 4, 10, 5, 6})
 	fmt.Println(r)
 }
 
@@ -3376,4 +3377,35 @@ func coinChange1(coins []int, amount int) int {
 		return -1
 	}
 	return dp[amount]
+}
+
+/*
+300. 最长递增子序列
+*https://leetcode.cn/problems/longest-increasing-subsequence/?envType=study-plan-v2&envId=top-interview-150
+*/
+func lengthOfLIS(nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+	dp := make([]int, len(nums))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = 1
+	}g
+	/**
+	10, 9, 2, 5, 3, 7, 101, 18
+	dp[i] = (x in 0...i-1 && nums[i] > nums[x]) max(dp[x]+1, dp[i])
+	*/
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+	}
+	fmt.Println(dp)
+	r := 0
+	for _, v := range dp {
+		r = max(r, v)
+	}
+	return r
 }
