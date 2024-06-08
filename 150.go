09e0edec250ddbc107e79e7fa411f8d283d9ff48
wangxinguo <wangxinguo@didiglobal.com>
2024-06-08 10:17:49 +0800

    添加

--- 150.go
+++ 150.go
@@ -3645,3 +3645,29 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
 	}
 	return f[n][m]
 }
+
+/*
+72. 编辑距离
+*https://leetcode.cn/problems/edit-distance/?envType=study-plan-v2&envId=top-interview-150
+*/
+func minDistance(word1 string, word2 string) int {
+	dp := make([][]int, len(word1)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(word2)+1)
+		dp[i][0] = i
+	}
+	for j := 0; j < len(dp[0]); j++ {
+		dp[0][j] = j
+	}
+
+	for i := 1; i <= len(word1); i++ {
+		for j := 1; j <= len(word2); j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = minM(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+			}
+		}
+	}
+	return dp[len(dp)-1][len(dp[0])-1]
+}
