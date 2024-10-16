96e68de9ada5d6b95889de7d87e4277131148013
wangxinguo <657908389@qq.com>
2024-10-16 10:16:29 +0800

    修改

--- 100.go
+++ 100.go
@@ -1569,6 +1569,7 @@ func subsetsImpl(nums []int, data *[]int, level int, r *[][]int) {
 /*
 39. 组合总和
 https://leetcode.cn/problems/combination-sum/description/
+题解：https://leetcode.cn/problems/subsets/solutions/7812/hui-su-python-dai-ma-by-liweiwei1419/
 */
 func combinationSum(candidates []int, target int) [][]int {
 	if len(candidates) < 1 {
@@ -1607,6 +1608,7 @@ func sumList(list []int) int {
 /*
 *40. 组合总和 IIg
 https://leetcode.cn/problems/combination-sum-ii/description/
+题解：https://leetcode.cn/problems/combination-sum-ii/solutions/14753/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-3/
 */
 func combinationSum2(candidates []int, target int) [][]int {
 	if len(candidates) < 1 {
@@ -1627,12 +1629,68 @@ func combinationSum2BT(candidates []int, data []int, target int, level int, r *[
 	} else if sumList(data) > target {
 		return
 	}
+	/*
+		这个方法最重要的作用是，可以让同一层级，不出现相同的元素。即
+		                  1
+		                 / \
+		                2   2  这种情况不会发生 但是却允许了不同层级之间的重复即：
+		               /     \
+		              5       5
+		                例2
+		                  1
+		                 /
+		                2      这种情况确是允许的
+		               /
+		              2
+
+		为何会有这种神奇的效果呢？
+		首先 cur-1 == cur 是用于判定当前元素是否和之前元素相同的语句。这个语句就能砍掉例1。
+		可是问题来了，如果把所有当前与之前一个元素相同的都砍掉，那么例二的情况也会消失。
+		因为当第二个2出现的时候，他就和前一个2相同了。
+
+		那么如何保留例2呢？
+		那么就用cur > begin 来避免这种情况，你发现例1中的两个2是处在同一个层级上的，
+		例2的两个2是处在不同层级上的。
+		***在一个for循环中，所有被遍历到的数都是属于一个层级的***。我们要让一个层级中，
+		必须出现且只出现一个2，那么就放过第一个出现重复的2，但不放过后面出现的2。
+		第一个出现的2的特点就是 cur == begin. 第二个出现的2 特点是cur > begin.
+	*/
 	for i := level; i < len(candidates); i++ {
 		if i > level && candidates[i-1] == candidates[i] {
 			continue
 		}
 		data = append(data, candidates[i])
 		combinationSum2BT(candidates, data, target, i+1, r)
+		data = data[:len(data)-1]
+	}
+	return
+}
+
+/*
+90. 子集 II
+https://leetcode.cn/problems/subsets-ii/
+*/
+func subsetsWithDup(nums []int) [][]int {
+	if len(nums) < 1 {
+		return [][]int{}
+	}
+	sort.Ints(nums)
+	r, data, level := make([][]int, 0), make([]int, 0), 0
+	subsetsWithDupBT1(nums, data, level, &r)
+	return r
+}
+
+func subsetsWithDupBT1(nums []int, data []int, level int, r *[][]int) {
+	tmp := make([]int, 0)
+	tmp = append(tmp, data...)
+	*r = append(*r, tmp)
+
+	for i := level; i < len(nums); i++ {
+		if i > level && nums[i] == nums[i-1] {
+			continue
+		}
+		data = append(data, nums[i])
+		subsetsWithDupBT1(nums, data, i+1, r)
 		data = data[:len(data)-1]
 	}
 	return
