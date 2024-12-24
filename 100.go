fa246843155d940ef865a82c18a60717ee5c93c1
wangxinguo <657908389@qq.com>
2024-12-24 11:15:20 +0800

    修改

--- 100.go
+++ 100.go
@@ -2679,8 +2679,18 @@ func climbStairs(n int) int {
 https://leetcode.cn/problems/pascals-triangle/description/?envType=study-plan-v2&envId=top-100-liked
 */
 func generate(numRows int) [][]int {
-	dp := make([][]int, numRows+1)
-	
+	if numRows == 0 {
+		return [][]int{}
+	}
+	dp := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		dp[i] = make([]int, i+1)
+		dp[i][0], dp[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+		}
+	}
+	return dp
 }
 
 /*
