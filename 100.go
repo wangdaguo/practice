6a700c294768d51f67e3c0bdb00e11ea9629da25
wangxinguo <657908389@qq.com>
2025-01-07 11:00:45 +0800

    修改

--- 100.go
+++ 100.go
@@ -3115,4 +3115,32 @@ func minDistance(word1 string, word2 string) int {
 			dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
 		}
 	*/
+	dp := make([][]int, len(word1)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(word2)+1)
+		dp[i][0] = i
+	}
+	for j := 0; j < len(dp[0]); j++ {
+		dp[0][j] = j
+	}
+	for i := 1; i <= len(word1); i++ {
+		for j := 1; j <= len(word2); j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = minM(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+			}
+		}
+	}
+	return dp[len(dp)-1][len(dp[0])-1]
+}
+
+func minM(x ...int) int {
+	min := x[0]
+	for i := 1; i < len(x); i++ {
+		if x[i] < min {
+			min = x[i]
+		}
+	}
+	return min
 }
