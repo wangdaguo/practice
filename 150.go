46040d7f07481c5472c84a0e5538328e4a29e07b
wangxinguo <657908389@qq.com>
2025-03-02 10:45:57 +0800

    修改

--- 150.go
+++ 150.go
@@ -1240,17 +1240,11 @@ func merge(intervals [][]int) [][]int {
 		}
 		return false
 	})
-	fmt.Println(intervals)
 	r, start, end := make([][]int, 0), intervals[0][0], intervals[0][1]
 	for i := 1; i < len(intervals); i++ {
-		// [1,3] [2,4]
-		if end >= intervals[i][0] && intervals[i][1] >= end {
+		if end >= intervals[i][0] && intervals[i][1] >= end { // [1,3] [2,4]
 			end = intervals[i][1]
-		} else if start <= intervals[i][0] && end >= intervals[i][1] { // [1,4] [1,3]
-			continue
-		} else if start >= intervals[i][0] && end <= intervals[i][1] { // [1,4] [1, 9]
-			end = intervals[i][1]
-		} else {
+		} else if end < intervals[i][0] {
 			r = append(r, []int{start, end})
 			start, end = intervals[i][0], intervals[i][1]
 		}
